156d1bf0075ee724ed028a68fa2ce8cc5df92072
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-12-26 18:20:41 +0500

    bug fix

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
+from unfold import admin as uadmin
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserModelAdmin(UserAdmin, uadmin.ModelAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    list_display = ["username", "first_name", "last_name", "middle_name", "city", "town", ]
+    model = User
+    fieldsets = (
+        ("Foydalanuvchini tahrirlash", {
+            "fields": ("username", "first_name", "last_name", "middle_name", "city", "town", "password", )
+        }), 
+    )
+    add_fieldsets = (
+        ("Yangi foydalanuvchi qo'shish", {
+            "fields": ("username", "password1", "password2", "first_name", "last_name", "last_name", "middle_name", "city", "town", )
+        }), 
+    )
